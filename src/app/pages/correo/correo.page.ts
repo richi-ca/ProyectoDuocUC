e41cd47ca55c27c5e9323f260970854c0275c237
KynDresh <and.osorio@duocuc.cl>
2023-11-17 07:08:33 -0300

    Corrección en las pages Correcto, Correo, Incorrecto, Ingreso, Pregunta

--- src/app/pages/correo/correo.page.ts
+++ src/app/pages/correo/correo.page.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Router, NavigationExtras} from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { DataBaseService } from 'src/app/services/data-base.service';
+import { Usuario } from 'src/app/model/usuario';
+import { async } from '@angular/core/testing';
+
 
 @Component({
   selector: 'app-correo',
@@ -12,9 +18,33 @@ import { IonicModule } from '@ionic/angular';
 })
 export class CorreoPage implements OnInit {
 
-  constructor() { }
+  public correo: string = '';
+
+  constructor(private bd: DataBaseService, private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
+  public ingresarPaginaValidarRespuestaSecreta(): void {
+    this.bd.leerUsuario(this.correo)
+      .then((usuario: Usuario) => {
+        if (!usuario) {
+          this.router.navigate(['/incorrecto']);
+        } else {
+          const navigationExtras: NavigationExtras = {
+            state: {
+              usuario: usuario
+            }
+          };
+          this.router.navigate(['/pregunta'], navigationExtras);
+        }
+      })
+      .catch(error => {
+        console.error('Error al leer usuario:', error);
+      });
+  }
+
+  ingresar() {
+    this.router.navigate(['/ingreso']);
+  }
 }
--- src/app/pages/pregunta/pregunta.page.ts
+++ src/app/pages/pregunta/pregunta.page.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { Router, NavigationExtras } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+import { DataBaseService } from 'src/app/services/data-base.service';
+import { Usuario } from 'src/app/model/usuario';
 
 @Component({
   selector: 'app-pregunta',
@@ -12,9 +16,38 @@ import { IonicModule } from '@ionic/angular';
 })
 export class PreguntaPage implements OnInit {
 
-  constructor() { }
+  public usuario: Usuario;
+  public respuesta: string = '';
+  public nombreUsuario: string = '';
+
+  constructor(private bd: DataBaseService, private authService: AuthService, private router: Router) { 
+    
+    
+  }
 
   ngOnInit() {
   }
 
+  public validarRespuestaSecreta(): void {
+    this.bd.leerUsuario(this.respuesta)
+      .then((usuario: Usuario) => {
+        if (!usuario) {
+          this.router.navigate(['/correcto']);
+        } else {
+          const navigationExtras: NavigationExtras = {
+            state: {
+              usuario: usuario
+            }
+          };
+          this.router.navigate(['/incorrecto'], navigationExtras);
+        }
+      })
+      .catch(error => {
+        console.error('Error al leer usuario:', error);
+      });
+  }
+
+  ingresar() {
+    this.router.navigate(['/ingreso']);
+  }
 }
