acaa5861d0f50910f5d6da807f0ed78e1e421cde
richi-ca <ric.castilloa@duocuc.cl>
2023-12-06 22:16:54 -0300

    ingreso.page.html se le añadio id: 'correo' y 'password'

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,14 +1,19 @@
+const correo = {
+  correo: "jjjjj@duocuc.cl",
+  // Otros datos de prueba...
+};
 
 describe('Verificar mi aplicaicon', () => {
   it('Verificar login con credenciales incorrectas', () => {
-    
+
     cy.wait(3000);
-    cy.visit('http://localhost:8100/').then(() => {
+    cy.visit('http://localhost:8100/ingreso').then(() => {
+      
       cy.get('#correo').invoke('val', 'jjjjj@duocuc.cl').should('have.value', 'jjjjj@duocuc.cl');
       cy.get('#password').invoke('val', '1111111');
-      cy.contains('Ingreso').click();
-      cy.intercept('ingreso').as('route').then(() => {
-        cy.get('ion-title').should('contain.text', 'Sistema de Asistencia DuocUC');
+      cy.contains('Ingresar').click();
+      cy.intercept('/ingreso').as('route').then(() => {
+        cy.get('ion-title').should('contain.text', 'Asistencia DUOC - Ingreso');
       });
     });
   });
@@ -16,26 +21,27 @@ describe('Verificar mi aplicaicon', () => {
   it('Verificar login con credenciales correctas', () => {
 
     cy.wait(3000);
-    cy.visit('http://localhost:8100/').then(() => {
-      cy.get('#correo').invoke('val', 'jperez@duocuc.cl');
-      cy.get('#password').invoke('val', '5678');
+    cy.visit('http://localhost:8100/ingreso').then(() => {
+      cy.get('#correo').invoke('val', '');
+      cy.get('#correo').type('atorres@duocuc.cl');
+      cy.get('#password').invoke('val', '');
+      cy.get('#password').type('1234');
       cy.contains('Ingresar').click();
       cy.intercept('./inicio').as('route').then(() => {
-        cy.get('ion-title').should('contain.text', 'Sistema de Asistencia DuocUC');
-        cy.get('#saludo').should('contain.text', '¡Bienvenido(a) Juan Pérez González!');
+        cy.get('ion-title').should('contain.text', 'Asistencia DUOC - Ingreso');
         cy.contains('Cerrar Sesión').click();
       });
     });
   });
 
-  it('Verificar inicio de sesión con credenciales incorrectas', () => {
-    cy.visit('http://localhost:8100/').then(() => {
-      cy.get('#correo').invoke('val', '');
-      cy.get('#correo').type('correo-inexistente@duocuc.cl');
-      cy.get('#password').invoke('val', '');
-      cy.get('#password').type('1234');
-      cy.contains('Ingresar').click();
-    });
-  })
+  // it('Verificar inicio de sesión con credenciales incorrectas', () => {
+  //   cy.visit('http://localhost:8100/').then(() => {
+  //     cy.get('#correo').invoke('val', '');
+  //     cy.get('#correo').type('correo-inexistente@duocuc.cl');
+  //     cy.get('#password').invoke('val', '');
+  //     cy.get('#password').type('1234');
+  //     cy.contains('Ingresar').click();
+  //   });
+  // })
 
 });
