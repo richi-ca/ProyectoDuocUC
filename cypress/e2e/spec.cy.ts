3224da6cc2c2e7d1339bb1ebfd64663f13ed581a
richi-ca <ric.castilloa@duocuc.cl>
2023-12-06 21:08:42 -0300

    cambios

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,12 +1,13 @@
+
 describe('Verificar mi aplicaicon', () => {
-  
   it('Verificar login con credenciales incorrectas', () => {
     
+    cy.wait(3000);
     cy.visit('http://localhost:8100/').then(() => {
-      cy.get('#correo').invoke('val', 'correo-inexistente@duocuc.cl');
+      cy.get('#correo').invoke('val', 'jjjjj@duocuc.cl').should('have.value', 'jjjjj@duocuc.cl');
       cy.get('#password').invoke('val', '1111111');
-      cy.contains('Ingresar').click();
-      cy.intercept('/ingresar').as('route').then(() => {
+      cy.contains('Ingreso').click();
+      cy.intercept('ingreso').as('route').then(() => {
         cy.get('ion-title').should('contain.text', 'Sistema de Asistencia DuocUC');
       });
     });
@@ -19,11 +20,22 @@ describe('Verificar mi aplicaicon', () => {
       cy.get('#correo').invoke('val', 'jperez@duocuc.cl');
       cy.get('#password').invoke('val', '5678');
       cy.contains('Ingresar').click();
-      cy.intercept('/inicio').as('route').then(() => {
+      cy.intercept('./inicio').as('route').then(() => {
         cy.get('ion-title').should('contain.text', 'Sistema de Asistencia DuocUC');
         cy.get('#saludo').should('contain.text', '¡Bienvenido(a) Juan Pérez González!');
         cy.contains('Cerrar Sesión').click();
       });
     });
   });
+
+  it('Verificar inicio de sesión con credenciales incorrectas', () => {
+    cy.visit('http://localhost:8100/').then(() => {
+      cy.get('#correo').invoke('val', '');
+      cy.get('#correo').type('correo-inexistente@duocuc.cl');
+      cy.get('#password').invoke('val', '');
+      cy.get('#password').type('1234');
+      cy.contains('Ingresar').click();
+    });
+  })
+
 });
--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -20,6 +20,7 @@ import './commands'
 // require('./commands')
 
 import { mount } from 'cypress/angular'
+import { Usuario } from 'src/app/model/usuario'
 
 // Augment the Cypress namespace to include type definitions for
 // your custom command.
